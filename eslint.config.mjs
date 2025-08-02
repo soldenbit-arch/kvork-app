650566efb0a790dd82449d49e8ba1e3bda6acbba
soldenbit-arch <soldenbit@gmail.com>
2025-08-02 07:03:21 +0300

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@next/next/no-img-element": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "react-hooks/exhaustive-deps": "off",
+            "@next/next/no-html-link-for-pages": "off",
+        },
+    },
+];
+
+export default eslintConfig;
